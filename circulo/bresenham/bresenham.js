66129aa480dabe8eeaa01ff936b789e27d458adb
yingyangtongxue <daniel.n.monteiro@unesp.br>
2022-11-30 17:03:09 -0300

    feat: add bresenham

--- circulo/bresenham/bresenham.js
+++ circulo/bresenham/bresenham.js
@@ -1,12 +1,31 @@
 import { context, canvas, start, end } from "../../canvas/canvas.js";
 
-context.beginPath();
-context.arc(95, 50, 40, 0, 2 * Math.PI);
-context.stroke();
-
+const draw = (xc, yc, x, y) =>{
+    context.fillRect(x+xc, y+yc, 1, 1)
+    context.fillRect(x+xc,-y+yc, 1, 1)
+    context.fillRect(-x+xc,-y+yc, 1, 1)
+    context.fillRect(-x+xc,y+yc, 1, 1)
+    context.fillRect(y+xc,x+yc, 1, 1)
+    context.fillRect(y+xc,-x+yc, 1, 1)
+    context.fillRect(-y+xc,-x+yc, 1, 1)
+    context.fillRect(-y+xc,x+yc, 1, 1)
+}
 
 const bresenham = (context, start, end) => {
-    console.log("precisa fazer")
+    const radius = Math.sqrt(Math.pow(end[0]-start[0],2) + Math.pow(end[1]-start[1],2))
+    let x = 0; let y = radius; let xc = start[0]+radius; let yc = start[1]+radius; let d = 3-2*radius;
+    draw(xc, yc, x, y)
+
+    while(x<=y){
+        if(d<=0){
+            d=d+(4*x)+6
+        }else{
+            d=d+(4*x)-(4*y)+10
+            y--
+        }
+        x++
+        draw(xc, yc, x, y)
+    }
 }
 
 canvas.addEventListener("mouseup", e =>{
--- reta/bresenham/bresenham.js
+++ reta/bresenham/bresenham.js
@@ -4,46 +4,44 @@ const bresenham = (context, start, end) => {
     const dx = Math.abs(end[0] - start[0])
     const dy = Math.abs(end[1] - start[1])
     const m = (end[1] - start[1]) / (end[0] - start[0]);
-    const actual = { x: 0, y: 0 };
+    const now = { x: 0, y: 0 };
 
-    if(dx > dy){ drawingX(context, start, end, actual, m) } else { drawingY(context, start, end, actual, m) }
+    if(dx > dy){ drawingX(context, start, end, now, m) } else { drawingY(context, start, end, now, m) }
 } 
 
 
-const drawingX = (context, start, end, actual, m) => {
-    console.log("x aqui")
-    actual.x = start[0];
+const drawingX = (context, start, end, now, m) => {
+    now.x = start[0];
     if (start[0] <= end[0]) {
         let limit = end[0] + 1;
-        while (actual.x < limit) {
-            actual.y = parseInt(m * (actual.x - start[0]) + start[1]); 
-            context.fillRect(actual.x, actual.y, 1, 1);
-            actual.x+=0.1;
+        while (now.x < limit) {
+            now.y = parseInt(m * (now.x - start[0]) + start[1]); 
+            context.fillRect(now.x, now.y, 1, 1);
+            now.x+=0.1;
         }
     } else {
-        while (actual.x > end[0]) {
-            actual.y = parseInt(m * (actual.x - start[0]) + start[1]);          
-            context.fillRect(actual.x, actual.y, 1, 1);
-            actual.x-=0.1;
+        while (now.x > end[0]) {
+            now.y = parseInt(m * (now.x - start[0]) + start[1]);          
+            context.fillRect(now.x, now.y, 1, 1);
+            now.x-=0.1;
         }
     }   
 }
 
-const drawingY = (context, start, end, actual, m) => {
-    console.log("y aqui")
-    actual.y = start[1];
+const drawingY = (context, start, end, now, m) => {
+    now.y = start[1];
     let limit = end[1] + 1;
     if (start[1] < limit) {
-        while (actual.y <= end[1]) {
-            actual.x = (actual.y - start[1]) / m + start[0];         
-            context.fillRect(actual.x, actual.y, 1, 1);
-            actual.y+=0.1;
+        while (now.y <= end[1]) {
+            now.x = (now.y - start[1]) / m + start[0];         
+            context.fillRect(now.x, now.y, 1, 1);
+            now.y+=0.1;
         }
     } else {
-        while (actual.y > end[1]) {
-            actual.x = (actual.y - start[1]) / m + start[0];
-            context.fillRect(actual.x, actual.y, 1, 1);
-            actual.y-=0.1;
+        while (now.y > end[1]) {
+            now.x = (now.y - start[1]) / m + start[0];
+            context.fillRect(now.x, now.y, 1, 1);
+            now.y-=0.1;
         }
     }   
 }
