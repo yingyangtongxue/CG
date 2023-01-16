2051c29096f223c77c137768b69d0ff7d20e30e0
yingyangtongxue <daniel@LAPTOP-5KGDVC8E>
2023-01-16 00:03:05 -0300

    feat: add z-buffer

--- z_buffer/constants.js
+++ z_buffer/constants.js
@@ -1,57 +1,53 @@
 export const SHAPES = [
     {
-        color: 0x00ff00, points: [
-            [0, 80, 0],
-            [20, 80, 0],
-            [20, 80, 40],
-            [0, 80, 40],
-        ]
+        id: 1,
+        type: 'plane',
+        color: 0x0000ff,
+        width: 10,
+        height: 10,
+        xInit: 10, 
+        yInit: 20,
+        zInit: 20,
+        yRotation: -30
     },
     {
-        color: 0xa52a2a, points: [
-            [100, 0, 0],
-            [120, 0, 0],
-            [120, 0, 40],
-            [100, 0, 40],
-        ]
+        id: 2,
+        type: 'plane',
+        color: 0xff0000,
+        width: 50,
+        height: 50,
+        xInit: 30, 
+        yInit: 20,
+        zInit: 10,
+        yRotation: 0
     },
     {
-        color: 0x40ee40, points: [
-            [0, 80, 40],
-            [20, 80, 40],
-            [20, 0, 40],
-            [0, 0, 40],
-        ]
+        id: 3,
+        type: 'circle',
+        color: 0xffff00, 
+        x: 30,
+        y: 50,
+        z: 10,
+        radius: 50
     },
     {
-        color: 0x00ff00, points: [
-            [0, 0, 0],
-            [20, 0, 0],
-            [20, 0, 40],
-            [0, 0, 40],
-        ]
+        id: 4,
+        type: 'circle',
+        color: 0x00ff00, 
+        x: 100,
+        y: 50,
+        z: 20,
+        radius: 30
     },
     {
-        color: 0xff0000, points: [
-            [20, 0, 0],
-            [100, 0, 0],
-            [100, 0, 40],
-            [20, 0, 40],
-        ]
-    },
-    {
-        color: 0x0000ff, points: [
-            [100, 10, 0],
-            [100, 0, 40],
-            [20, 80, 40],
-            [20, 80, 0],
-        ]
-    },
-    {
-        color: 0xffff00, points: [
-            [20, 0, 0],
-            [100, 0, 0],
-            [20, 80, 0],
-        ]
+        id: 5,
+        type: 'cube',
+        color: 0xffffff,
+        width: 40,
+        height: 40,
+        xInit: 0, 
+        yInit: 0,
+        zInit: -20,
+        depth: 40
     },
 ]
--- z_buffer/zbuffer.js
+++ z_buffer/zbuffer.js
@@ -3,81 +3,109 @@ import { SHAPES } from "./constants.js";
 const scene = new THREE.Scene();
 
 const camera = new THREE.OrthographicCamera(-560, 560, 280, -280, 1, 1000)
-camera.position.set(0, 0, 100)
+camera.position.set(0, 0, 50)
 scene.add(camera)
 
 const renderer = new THREE.WebGLRenderer({antialiases: true})
 renderer.setSize(1080, 560)
 document.querySelector('#canvas-container').appendChild(renderer.domElement)
 
-let rotationX = 20
-let rotationY = -20
-let rotationZ = 0
+const allGeometries = {}
 
-const allGeometries = []
+const plotCircle = circle => {
+    const { id, radius, x, y, z, color } = circle
+    const geometry = new THREE.CircleGeometry(radius, 32)
+    const material = new THREE.MeshBasicMaterial({color: color})
+    const mesh = new THREE.Mesh(geometry, material)
+
+    geometry.translate(x, y, z)
+    scene.add(mesh)
+    allGeometries[`${id}`] = geometry
+}
+
+const plotPlane = plane => {
+    const { id, color, width, height, xInit, yInit, zInit, yRotation } = plane
+    const shape = new THREE.Shape()
+
+    shape.moveTo(xInit, yInit)
+    shape.lineTo(xInit+width, yInit)
+    shape.lineTo(xInit+width, yInit+height)
+    shape.lineTo(xInit, yInit+height)
+    shape.lineTo(xInit, yInit)
+    
+    const geometry = new THREE.ExtrudeGeometry(shape, {depth: 0, bevelEnabled: false})
+    const material = new THREE.MeshBasicMaterial({color: color})
+    const mesh = new THREE.Mesh(geometry, material)
+
+    allGeometries[`${id}`] = geometry
+    geometry.translate(xInit, yInit, zInit)
+    geometry.rotateY(yRotation * (Math.PI / 180))
+    scene.add(mesh)
+}
+
+const plotCube = cube => {
+    const { id, color, width, height, depth, xInit, yInit } = cube
+    const geometry = new THREE.BoxGeometry(width, height, depth)
+    const material = new THREE.MeshBasicMaterial({color: color})
+    const mesh = new THREE.Mesh(geometry, material)
+
+    allGeometries[`${id}`] = geometry
+    geometry.translate(xInit, yInit, 0)
+    scene.add(mesh)
+}
+
+const plotter = {
+    'circle': plotCircle,
+    'plane': plotPlane,
+    'cube': plotCube
+}
 
 const plotObject = () => {
-    SHAPES.forEach(shape => {
-        const { color, points } = shape
-        const [ xBegin, yBegin, _ ] = points[0]
-        const allZs = points.map(point => point[2])
-        const maxZ = allZs.reduce((z, acc) => Math.max(z, acc))
-        const minZ = allZs.reduce((z, acc) => Math.min(z, acc))
-        const depth = Math.max(maxZ - minZ, 0)
-        const path = new THREE.Shape()
-
-        path.moveTo(xBegin, yBegin)
-        for(let i = 1; i < points.length; i++) {
-            const [ x, y, _ ] = points[i]
-            path.lineTo(x, y)
-        }
-        path.lineTo(xBegin, yBegin)
-
-        const geometry = new THREE.ExtrudeGeometry(path, {depth: depth, bevelEnabled: false})
-        allGeometries.push(geometry)
-        geometry.rotateX(rotationX * (Math.PI / 180))
-        geometry.rotateY(rotationY * (Math.PI / 180))
-        geometry.rotateZ(rotationZ * (Math.PI / 180))
-        const material = new THREE.MeshBasicMaterial({color: color})
-        const mesh = new THREE.Mesh(geometry, material)
-
-        scene.add(mesh)
-        renderer.render(scene, camera)
-    })
+    SHAPES.forEach(shape => plotter[shape.type](shape))
+    renderer.render(scene, camera)
 }
 
 plotObject()
 
-const rotateObject = (x, y, z) => {
-    allGeometries.forEach(geometry => {
-        geometry.rotateX(x * (Math.PI / 180))
-        geometry.rotateY(y * (Math.PI / 180))
-        geometry.rotateZ(z * (Math.PI / 180))
-    })
-    renderer.render(scene, camera)
-}
+const degreeToRadian = degree => degree * (Math.PI / 180)
 
-document.querySelector("#rotation-x-minus").addEventListener('click', () => {
-    rotationX += 10
-    rotateObject(10, 0, 0)
+document.querySelector('#object-1-down').addEventListener('click', () => {
+    allGeometries['1'].rotateZ(degreeToRadian(10))
+    renderer.render(scene, camera)
 })
-document.querySelector("#rotation-x-plus").addEventListener('click', () => {
-    rotationX -= 10
-    rotateObject(-10, 0, 0)
+document.querySelector('#object-1-up').addEventListener('click', () => {
+    allGeometries['1'].rotateZ(degreeToRadian(-10))
+    renderer.render(scene, camera)
 })
-document.querySelector("#rotation-y-minus").addEventListener('click', () => {
-    rotationY += 10
-    rotateObject(0, 10, 0)
+document.querySelector('#object-2-down').addEventListener('click', () => {
+    allGeometries['2'].rotateZ(degreeToRadian(10))
+    renderer.render(scene, camera)
 })
-document.querySelector("#rotation-y-plus").addEventListener('click', () => {
-    rotationY -= 10
-    rotateObject(0, -10, 0)
+document.querySelector('#object-2-up').addEventListener('click', () => {
+    allGeometries['2'].rotateZ(degreeToRadian(-10))
+    renderer.render(scene, camera)
 })
-document.querySelector("#rotation-z-minus").addEventListener('click', () => {
-    rotationZ += 10
-    rotateObject(0, 0, 10)
+document.querySelector('#object-3-down').addEventListener('click', () => {
+    allGeometries['3'].rotateZ(degreeToRadian(10))
+    renderer.render(scene, camera)
 })
-document.querySelector("#rotation-z-plus").addEventListener('click', () => {
-    rotationZ -= 10
-    rotateObject(0, 0, -10)
+document.querySelector('#object-3-up').addEventListener('click', () => {
+    allGeometries['3'].rotateZ(degreeToRadian(-10))
+    renderer.render(scene, camera)
+})
+document.querySelector('#object-4-down').addEventListener('click', () => {
+    allGeometries['4'].rotateZ(degreeToRadian(10))
+    renderer.render(scene, camera)
+})
+document.querySelector('#object-4-up').addEventListener('click', () => {
+    allGeometries['4'].rotateZ(degreeToRadian(-10))
+    renderer.render(scene, camera)
+})
+document.querySelector('#object-5-down').addEventListener('click', () => {
+    allGeometries['5'].rotateZ(degreeToRadian(10))
+    renderer.render(scene, camera)
+})
+document.querySelector('#object-5-up').addEventListener('click', () => {
+    allGeometries['5'].rotateZ(degreeToRadian(-10))
+    renderer.render(scene, camera)
 })
